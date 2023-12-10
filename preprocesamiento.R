268fe8de1a5afaf0f7f604287ce70b269bd9ebb3
José Saint Germain <josesg998@gmail.com>
2023-12-10 17:16:55 -0300

    agrego procesamiento y feature engineering de estaciones

--- preprocesamiento.R
+++ preprocesamiento.R
@@ -107,11 +107,92 @@ estaciones$estacion <- gsub("Ú","U",estaciones$estacion)
 estaciones$estacion <- gsub("1°","I",estaciones$estacion)
 estaciones$estacion <- gsub("Ü","U",estaciones$estacion)
 
+# agregamos latitud y longitud
+estaciones <- fread("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/sbase/subte-estaciones/estaciones-de-subte.csv")
 
-# separa la columna estacion por el delimitador " - "
+estaciones <- estaciones |> tidyr::separate(col = "estacion",into = c("estacion","estacion_nueva"),sep = "( - )|( -)")
 
-estaciones <- estaciones  |>  tidyr::separate(estacion, c("estacion", "nuevo_nombre"), sep = " - ", remove = FALSE)
-estaciones$LINEA_ESTACION <- paste(estaciones$linea,estaciones$estacion,sep="_")
 
-mol_total <- merge(mol_total,estaciones,by="LINEA_ESTACION",all.x=T)
+estaciones$en_tabla <-  estaciones$estacion %in% mol_total$ESTACION
 
+estaciones$estacion <- gsub("Ü","U",estaciones$estacion)
+estaciones$estacion <- gsub("1°","I",estaciones$estacion)
+estaciones$estacion <- gsub("É","E",estaciones$estacion)
+estaciones$estacion <- gsub("Ó","O",estaciones$estacion)
+estaciones$estacion <- gsub("Á","A",estaciones$estacion)
+estaciones$estacion <- gsub("Í","I",estaciones$estacion)
+
+estaciones$estacion <- gsub("R\\.","",estaciones$estacion)
+estaciones$estacion <- gsub("AV\\.","AVENIDA",estaciones$estacion)
+estaciones$estacion <- gsub("MORENO","MARIANO MORENO",estaciones$estacion)
+estaciones$estacion <- gsub("C\\.","CARLOS",estaciones$estacion)
+estaciones$estacion <- gsub("ALMAGRO","MEDRANO",estaciones$estacion)
+estaciones$estacion <- gsub("PLAZA DE MISERERE","PLAZA MISERERE",estaciones$estacion)
+estaciones$estacion <- gsub("BELGRANO","GENERAL BELGRANO",estaciones$estacion)
+estaciones$estacion <- gsub("JOSE MARIA MARIANO MORENO","JOSE MARIA MORENO",estaciones$estacion)
+estaciones$estacion <- gsub("SAN MARTIN","GENERAL SAN MARTIN",estaciones$estacion)
+estaciones$estacion <- gsub("PLAZA DE LOS VIRREYES","PZA. DE LOS VIRREYES",estaciones$estacion)
+estaciones$estacion <- gsub("DE LOS INCAS","LOS INCAS",estaciones$estacion)
+estaciones$estacion <- gsub("PARQUE PATRICIOS","PATRICIOS",estaciones$estacion)
+estaciones$estacion <- gsub("JUAN MANUEL DE ROSAS","ROSAS",estaciones$estacion)
+estaciones$estacion <- gsub("SAN JOSE DE FLORES","FLORES",estaciones$estacion)
+
+estaciones$LINEA_ESTACION <- paste0(estaciones$linea,"_",estaciones$estacion)
+estaciones <- estaciones[,c("id","LINEA_ESTACION","lat","long")]
+
+
+# detectar si la estación está dentro de un barrio
+barrios <- fread("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-educacion/barrios/barrios.csv")
+
+# Crea objetos sf para los dataframes
+estaciones <- st_as_sf(estaciones, coords = c("long", "lat"), crs = 4326)
+barrios <- st_as_sf(barrios, wkt = "WKT", crs = 4326)
+
+# Realiza la operación de punto en polígono para asignar el barrio
+estaciones <- st_join(estaciones, barrios)
+estaciones <- estaciones_sf[,c("id","LINEA_ESTACION","BARRIO","COMUNA")]
+
+# escuelas
+escuelas <- st_read("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-educacion/establecimientos-educativos/escuelas-verdes-wgs84.geojson")
+hospitales <- st_read("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-salud/hospitales/hospitales.geojson")
+universidades <- fread("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-educacion/universidades/universidades.csv")
+centros_culturales <- st_read("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/ministerio-de-cultura/espacios-culturales/espacios-culturales.geojson")
+puntos_verdes <- st_read("https://cdn.buenosaires.gob.ar/datosabiertos/datasets/agencia-de-proteccion-ambiental/puntos-verdes/puntos-verdes.geojson")
+
+universidades <- universidades[,list(univ_c,universida,unidad_aca,direccion_norm,lat,long)]
+# remover duplicados
+universidades <- universidades[!duplicated(universidades$direccion_norm),]
+universidades <- st_as_sf(universidades, coords = c("long", "lat"), crs = 4326)
+
+centros_culturales <- centros_culturales[!duplicated(centros_culturales$DIRECCION),]
+
+# Función para contar el número de escuelas dentro de 300 metros de una estación
+contar_edificios <- function(mapa,distancia) {
+  buffer <- st_buffer(estaciones, dist = distancia)  # Crea un buffer de 300 metros alrededor de la estación
+  conteo <- lengths(st_intersects(buffer, mapa))
+  return(conteo)
+}
+
+estaciones$universidades <- contar_edificios(universidades,500)
+estaciones$escuelas <- contar_edificios(escuelas,500)
+estaciones$hospitales <- contar_edificios(hospitales,500)
+estaciones$centros_culturales <- contar_edificios(centros_culturales,500)
+estaciones$puntos_verdes <- contar_edificios(puntos_verdes,500)
+
+
+buffer <- st_buffer(estaciones, dist = 500)
+
+ggplot()+
+  geom_sf(data=barrios)+
+  geom_sf(data=buffer)+
+  geom_sf(data=escuelas,color="red")+
+  geom_sf(data=universidades,color="orange")+
+  #geom_sf(data=centros_culturales,color="purple",alpha=.2)+
+  geom_sf(data=hospitales,color="blue")+
+  geom_sf(data=puntos_verdes,color="green")+
+  #add legend
+  #scale_color_manual(values=c("orange","blue","red","green"),labels=c("Universidades","Hospitales","Escuelas","Puntos verdes"))+
+  theme_void()+
+  theme(legend.position = "bottom")
+
+fwrite(estaciones,"estaciones.csv.gz")
